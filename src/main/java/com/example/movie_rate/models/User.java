a0ff3370488e3b6a997afe0dc5482ea476f96016
trentycha <trentycharlene@gmail.com>
2026-01-26 23:29:01 +0100

    Add user entity and first migration

---
+++ src/main/java/com/example/movie_rate/models/User.java
@@ -0,0 +1,83 @@
+package com.example.movie_rate.models;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String mail;
+
+    @Column(nullable = false)
+    private String password;
+
+//    @OneToMany(mappedBy = "user", orphanRemoval = true)
+//    private List<Film> films = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", orphanRemoval = true)
+//    private List<Role> roles = new ArrayList<>();
+
+
+    public User(){}
+
+    public User(String username, String mail, String password) {
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String setUsername() {
+        return this.username = username;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String setMail() {
+        return this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String setPassword() {
+        return this.password = password;
+    }
+
+//    public List<Film> getFilms() {
+//        return films;
+//    }
+//
+//    public List<Film> setFilms() {
+//        return this.films = films;
+//    }
+//
+//    public List<Role> getRoles() {
+//        return roles;
+//    }
+//
+//    public List<Role> setRoles() {
+//        return this.roles = roles;
+//    }
+
+}
