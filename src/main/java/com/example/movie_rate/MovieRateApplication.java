49658690afc18deecc47cfb8d474dfdf1762e761
trentycha <trentycharlene@gmail.com>
2026-01-26 22:20:02 +0100

    Initial commit

---
+++ src/main/java/com/example/movie_rate/MovieRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movie_rate/MovieRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
