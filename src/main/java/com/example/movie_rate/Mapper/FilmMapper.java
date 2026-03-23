413d9549f80d1d01deb9c427d795eb5e01eb0f8b
trentycha <trentycharlene@gmail.com>
2026-03-23 20:45:52 +0100

    UserMapper and FilmMapper added

---
+++ src/main/java/com/example/movie_rate/Mapper/FilmMapper.java
@@ -0,0 +1,25 @@
+package com.example.movie_rate.Mapper;
+import com.example.movie_rate.models.Film;
+import com.example.movie_rate.DTO.FilmDTO;
+
+public class FilmMapper {
+
+    public FilmDTO toFilmDto (Film film) {
+
+        return new FilmDTO(
+                film.getId(),
+                film.getTitle(),
+                film.getCreatedAt(),
+                film.getUpdatedAt(),
+                film.getDirector(),
+                film.getMainActors(),
+                film.getDuration(),
+                film.getYear(),
+                film.getComment(),
+                film.getAnecdote()
+        );
+
+    }
+
+}
+
---
+++ src/main/java/com/example/movie_rate/Mapper/userMapper.java
@@ -0,0 +1,17 @@
+package com.example.movie_rate.Mapper;
+import com.example.movie_rate.models.User;
+import com.example.movie_rate.DTO.UserDTO;
+
+public class userMapper {
+
+    public UserDTO toUserDto (User user) {
+
+        return new UserDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getMail()
+        );
+
+    }
+
+}
